3a5fd8b70ea2c256797de9432afa36ce3030828a
ASiCo1116 <b05611046@ntu.edu.tw>
2024-01-29 18:53:38 +0800

    [0129] add mapbox

--- seeds/index.js
+++ seeds/index.js
@@ -15,13 +15,20 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 200; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: "65ae41ce7e9b576ed09edff0",
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: `https://source.unsplash.com/random/300x300?camping,${i * 2}`,
